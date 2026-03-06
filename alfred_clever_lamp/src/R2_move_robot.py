c65aade9d104201ea0ec93aa1fe7c51ce31e4a42
GRINGOLOCO7 <gorlando.ieu2022@student.ie.edu>
2026-03-06 11:03:50 +0100

    fix velocities and ne instructions

--- alfred_clever_lamp/src/R2_move_robot.py
+++ alfred_clever_lamp/src/R2_move_robot.py
@@ -11,7 +11,7 @@ def start_robot():
     rospy.loginfo("\nstart robot")
     bot = InterbotixManipulatorXS("wx250s", "arm", "gripper", init_node=False) # /home/gringo/interbotix_ws/src/interbotix_ros_toolboxes/interbotix_xs_toolbox/interbotix_xs_modules/src/interbotix_xs_modules/arm.py
     bot.arm.set_ee_pose_components(x=0, y=0.15, z=0.25, pitch=0.75, moving_time=2.5)
-    bot.arm.set_ee_pose_components(x=-0.15, y=0.15, z=0.25, pitch=-0.3, moving_time=0.9)
+    bot.arm.set_ee_pose_components(x=-0.15, y=0.15, z=0.25, pitch=-0.3, moving_time=1.3)
     bot.arm.set_ee_pose_components(x=-0.15, y=0.15, z=0.25, pitch=0.3, moving_time=0.9)
     rospy.loginfo("\ndone robot")
 
@@ -24,14 +24,14 @@ def out_boundary(x, y):
     boundary_y = 0.30 * math.sin(angle)
     #rospy.loginfo(f"\nPoint is out of boundary. Moving to boundary point: x={boundary_x:.2f}, y={boundary_y:.2f}")
     # Perform a 'no' shaking motion
-    bot.arm.set_ee_pose_components(x=boundary_x, y=boundary_y, z=0.3, pitch=1.5, yaw=0.2, moving_time=0.5)
-    bot.arm.set_ee_pose_components(x=boundary_x, y=boundary_y, z=0.3, pitch=1.5, yaw=-0.2, moving_time=0.5)
+    bot.arm.set_ee_pose_components(x=boundary_x, y=boundary_y, z=0.3, pitch=1.5, yaw=0.2, moving_time=0.45)
+    bot.arm.set_ee_pose_components(x=boundary_x, y=boundary_y, z=0.3, pitch=1.5, yaw=-0.2, moving_time=0.45)
     #bot.arm.set_ee_pose_components(x=boundary_x, y=boundary_y, z=0.3, pitch=1.5, yaw=0, moving_time=0.5)
 
 
 if __name__ == '__main__':
     x_offset = 0.25
-    y_offset = 0.1
+    y_offset = 0.0
     rospy.init_node('move_robot')
 
     tfBuffer = tf2_ros.Buffer()
@@ -61,7 +61,7 @@ if __name__ == '__main__':
             out_boundary(x, y)
         else:
             start_time = time.time()
-            bot.arm.set_ee_pose_components(x=x, y=y, z=0.3, pitch=1.5, yaw=0, moving_time=1.5)
+            bot.arm.set_ee_pose_components(x=x, y=y, z=0.3, pitch=1.5, yaw=0, moving_time=1.8)
             end_time = time.time() - start_time
             #rospy.loginfo(f"\nTime taken to move: {end_time:.2f} seconds")
         rate.sleep()
